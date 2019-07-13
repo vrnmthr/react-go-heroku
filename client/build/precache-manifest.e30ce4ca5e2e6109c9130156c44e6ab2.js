c2a15e762e67773ce6715663a7c5cc49533efc41
vrnmthr <varun.mathur@live.com>
2019-07-13 12:21:21 +0530

    added chunks

---
+++ client/build/precache-manifest.e30ce4ca5e2e6109c9130156c44e6ab2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1d683b4b2336ead044126488d26df61",
+    "url": "/index.html"
+  },
+  {
+    "revision": "19862603c6d53ac7574c",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "b2950a9c76b13afceb18",
+    "url": "/static/js/2.de83225a.chunk.js"
+  },
+  {
+    "revision": "19862603c6d53ac7574c",
+    "url": "/static/js/main.c2e7c7a1.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
